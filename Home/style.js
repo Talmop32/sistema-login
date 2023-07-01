0bac0790736acadd5603f068e02e49c0b436fd56
Talmop32 <talmop32@gmail.com>
2023-07-01 14:35:24 -0300

    commit pages

---
+++ Home/style.js
@@ -0,0 +1,12 @@
+import style from "styled-components";
+
+export const Container = style.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+height: 100vh;
+gap: 20;
+`;
+
+export const Title = style.h2``;
